e98f56f18d224e8814a4ae7c6c8a28c4fe5a755f
Monica <msmonicaking@gmail.com>
2022-05-15 22:13:36 -0700

    added nodemon

--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ const express = require("express");
 
 const app = express();
 
+// anytime we get a get method toward the passed URL, it will run this route
+app.get("/getbooks", async(request, response) => {
+    console.log("get books");
+
+    try {
+        const book = await db.query(
+            //
+        )
+    }
+});
+
 // sets port to value defined in .env file
 const port = process.env.PORT || 3003;
 app.listen(port, () => {
