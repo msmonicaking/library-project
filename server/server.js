8ac522cde6ed2bcdb1bb211101eded2640e43cff
Monica <msmonicaking@gmail.com>
2022-05-15 21:53:25 -0700

    added environment variables

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-const express = require("express")
+require('dotenv').config();
+console.log(process.env); // remove this after you've confirmed it working
 
+const express = require("express");
 
+const app = express();
 
-
-const app = express()
-
-app.listen(3000, () => {
-    console.log("server is up & listening on port 3000")
-})
+// sets port to value defined in .env file
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+    console.log(`server is up & listening on port ${port}`);
+});
