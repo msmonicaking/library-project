b2895bd02212cfa1132a2a6f78771068ad67120c
ht--j <huantong6652@gmail.com>
2022-05-17 22:51:20 -0700

    added sql queries and index.js

---
+++ server/db/database.sql
@@ -0,0 +1,73 @@
+CREATE DATABASE project111;
+
+CREATE TABLE author(
+                       id SERIAL,
+                       firstname VARCHAR(255) NOT NULL,
+                       lastname VARCHAR(255) NOT NULL,
+
+                       PRIMARY KEY (id)
+);
+
+CREATE TABLE category(
+                         id SERIAL,
+                         name VARCHAR(255) NOT NULL,
+
+                         PRIMARY KEY (id)
+);
+
+CREATE TABLE catalogcard(
+                            id SERIAL,
+                            title VARCHAR(255) NOT NULL,
+                            authorid INTEGER,
+                            categoryid INTEGER,
+                            description VARCHAR(255),
+                            isbn VARCHAR(255) NOT NULL,
+
+                            PRIMARY KEY (id),
+                            FOREIGN KEY (categoryid) REFERENCES category(id) DEFERRABLE INITIALLY DEFERRED,
+                            FOREIGN KEY (authorid) REFERENCES author(id) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE book(
+                     id SERIAL,
+                     catelogid INTEGER,
+                     isdeleted BOOLEAN NOT NULL,
+
+                     PRIMARY KEY (id),
+                     FOREIGN KEY (catelogid) REFERENCES catalogcard(id) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE usertype(
+                         id SERIAL,
+                         type VARCHAR(255) NOT NULL,
+
+                         PRIMARY KEY (id)
+);
+
+CREATE TABLE useraccount(
+                            id SERIAL,
+                            firstname VARCHAR(255) NOT NULL,
+                            lastname VARCHAR(255) NOT NULL,
+                            username VARCHAR(255) NOT NULL,
+                            usertypeid INTEGER NOT NULL,
+                            email VARCHAR(255) NOT NULL,
+                            phonenumber VARCHAR(255) NOT NULL,
+                            isdeleted boolean NOT NULL DEFAULT false,
+
+                            PRIMARY KEY (id),
+                            FOREIGN KEY (usertypeid) REFERENCES usertype(id) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE checkout(
+                         id SERIAL,
+                         useraccountid INTEGER NOT NULL,
+                         bookid INTEGER NOT NULL,
+                         dateout DATE NOT NULL,
+                         returnby DATE NOT NULL,
+                         isreturned BOOLEAN NOT NULL,
+                         returndate DATE NOT NULL,
+
+                         PRIMARY KEY (id),
+                         FOREIGN KEY (useraccountid) REFERENCES useraccount(id) DEFERRABLE INITIALLY DEFERRED,
+                         FOREIGN KEY (bookid) REFERENCES book(id) DEFERRABLE INITIALLY DEFERRED
+);
